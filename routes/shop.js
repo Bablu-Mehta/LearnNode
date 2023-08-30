c06e15f625048fad6b356430582b7390e2a88715
Bablu Mehta <babloomehta895012@gmail.com>
2023-08-30 13:01:41 +0530

    Learn Pug Templates

--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,22 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path')
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/',(req, res, next)=>{
-    res.sendFile(path.join(rootDir,'views', 'shop.html'));
+
+    const products = adminData.products;
+
+
+    res.render('shop',{prods:products, PageTitle:'Shop', path: '/'}); //this is where we are using the template engine and we dont have to 
+                        //define the location and the type because we already defined it in the
+                        //app.js file to which type of template we are using and where is the location
+                        //of the file with the help of app.set
+
+    // console.log('shop.js', adminData.products);
+    // res.sendFile(path.join(rootDir,'views', 'shop.html'));
 });
 
 
