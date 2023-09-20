0fc9a643ef1429cf56242213e9094398253669c2
Bablu Mehta <babloomehta895012@gmail.com>
2023-09-20 09:21:26 +0530

    almost done with models

--- Controller/products.js
+++ Controller/products.js
@@ -1,5 +1,5 @@
 
-const products = [];
+const Product = require('../models/product')
 
 exports.getAddProducts = (req, res, next) => {
     res.render("add-product", {
@@ -12,19 +12,24 @@ exports.getAddProducts = (req, res, next) => {
   }
 
   exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect("/");
   }
 
   exports.getProducts = (req, res, next) => {
-    res.render("shop", {
-      prods: products,
-      PageTitle: "Shop",
-      path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    }); //this is where we are using the template engine and we dont have to
+    Product.fetchAll((products)=>{
+      res.render("shop", {
+        prods: products,
+        PageTitle: "Shop",
+        path: "/",
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+      });
+    });
+    
+     //this is where we are using the template engine and we dont have to
     //define the location and the type because we already defined it in the
     //app.js file to which type of template we are using and where is the location
     //of the file with the help of app.set
