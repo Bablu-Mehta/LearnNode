6e41a3f6dd58e94b4d3a484b4f9bd5e4256f2bc2
Bablu Mehta <babloomehta895012@gmail.com>
2023-09-21 07:48:21 +0530

    Complete the Models part of nodejs learning

--- models/product.js
+++ models/product.js
@@ -1,23 +1,31 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports = class Product {
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = cb =>{
+  
+fs.readFile(p, (err, fileContent)=>{
+    if(err)
+    {
+        return cb([]);
+    }
+    cb( JSON.parse(fileContent));
+});
+}
+
+module.exports = class Product 
+{
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p,(err, fileContent)=>{
-        let products = [];
-        if(!err)
-        {
-            products = JSON.parse(fileContent);
-        }
+    getProductsFromFile(products => {
         products.push(this);
         fs.writeFile(p, JSON.stringify(products),(err)=>{
             console.log(err);
@@ -25,18 +33,8 @@ module.exports = class Product {
     });
   }
 
-  static fetchAll(cb) {
-    const p = path.join(
-        path.dirname(require.main.filename),
-        "data",
-        "products.json"
-      );
-    fs.readFile(p, (err, fileContent)=>{
-        if(err)
-        {
-            cb([]);
-        }
-        cb( JSON.parse(fileContent));
-    });
+  static fetchAll(cb) 
+  {
+getProductsFromFile(cb);
   }
 };
