eb2b4e9a8158a24d05f3f437f6e8b2f553fe280e
Bablu Mehta <babloomehta895012@gmail.com>
2023-09-30 08:50:44 +0530

    Rendering the product detail view

--- Controller/shop.js
+++ Controller/shop.js
@@ -1,63 +1,61 @@
+const Product = require("../models/product");
 
-const Product = require('../models/product')
-
-
-
-  exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products)=>{
-      res.render("shop/product-list", {
-        prods: products,
-        PageTitle: "All Products",
-        path: "/products",
-      });
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      PageTitle: "All Products",
+      path: "/products",
     });
-    
-     //this is where we are using the template engine and we dont have to
-    //define the location and the type because we already defined it in the
-    //app.js file to which type of template we are using and where is the location
-    //of the file with the help of app.set
-  
-    // console.log('shop.js', adminData.products);
-    // res.sendFile(path.join(rootDir,'views', 'shop.html'));
-  }
+  });
 
-  exports.getProduct = (req, res, next) =>{
-    const prodId = req.params.productId;
-    Product.findById(prodId, product =>{
-      console.log(product);
-    })
-    res.redirect('/');
-  }
+  //this is where we are using the template engine and we dont have to
+  //define the location and the type because we already defined it in the
+  //app.js file to which type of template we are using and where is the location
+  //of the file with the help of app.set
 
-  exports.getIndex = (req, res, next) => {
-    Product.fetchAll((products)=>{
-      res.render("shop/index", {
-        prods: products,
-        PageTitle: "Shop",
-        path: "/",
-      });
+  // console.log('shop.js', adminData.products);
+  // res.sendFile(path.join(rootDir,'views', 'shop.html'));
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product: product,
+      PageTitle: product.title,
+      path: '/products'
     });
-  }
+  });
+};
 
-  exports.getCart = (req, res, next) =>{
-    res.render('shop/cart', {
-      path: '/cart',
-      PageTitle:'Your Cart'
-    })
-  }
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      PageTitle: "Shop",
+      path: "/",
+    });
+  });
+};
 
-  exports.getOrders = (req, res, next) =>{
-    res.render('shop/orders', {
-      path: '/orders',
-      PageTitle:'Your Orders'
-    })
-  }
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    path: "/cart",
+    PageTitle: "Your Cart",
+  });
+};
 
-  exports.getCheckout = (res, req, next) =>{
-    res.render('shop/checkout', {
-      path:'/checkout',
-      PageTitle: 'Checkout'
-    })
-  }
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    PageTitle: "Your Orders",
+  });
+};
 
-  
+exports.getCheckout = (res, req, next) => {
+  res.render("shop/checkout", {
+    path: "/checkout",
+    PageTitle: "Checkout",
+  });
+};
