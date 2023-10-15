dcf6d91789a2ac4ff19beb43aeec8d9eccb8e25e
Bablu Mehta <babloomehta895012@gmail.com>
2023-10-15 17:06:39 +0530

    resolved the error

--- Controller/admin.js
+++ Controller/admin.js
@@ -12,7 +12,7 @@ exports.getAddProducts = (req, res, next) => {
   exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
-    const price = req.body.price;
+    const price = +req.body.price;
     const description = req.body.description;
 
     const product = new Product(null, title, imageUrl, description, price);
@@ -34,6 +34,7 @@ exports.getAddProducts = (req, res, next) => {
       {
         return res.redirect('/')
       }
+      
       res.render("admin/edit-product", {
         PageTitle: "Edit Product",
         path: "/admin/edit-product",
