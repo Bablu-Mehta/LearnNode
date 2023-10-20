11fddc431cce3aab7b4ab7497d39d073e688866b
Bablu Mehta <babloomehta895012@gmail.com>
2023-10-20 08:30:50 +0530

    Displaying cart items on the cart page

--- Controller/shop.js
+++ Controller/shop.js
@@ -41,10 +41,25 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    path: "/cart",
-    PageTitle: "Your Cart",
+  Cart.getCart(cart =>{
+    Product.fetchAll(products =>{
+      const cartProducts = [];
+      for (const product of products){
+        const cartProductData = cart.products.find(prod => prod.id === product.id);
+        if(cartProductData)
+        {
+          cartProducts.push({prodcutData: product, qty: cartProductData.qty});
+        }
+      }
+      res.render("shop/cart", {
+        path: "/cart",
+        PageTitle: "Your Cart",
+        products: cartProducts
+      });
+    });
+    
   });
+ 
 };
 
 exports.postCart = (req, res, next) => {
